bfb12254850de66151a7f72b0fa04898d3ba39a0
Emirayka <emirayka@protonmail.com>
2020-07-03 21:01:25 +0300

    Initial commit.

---
+++ dist/index.umd.js
@@ -0,0 +1,1 @@
+!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t():"function"==typeof define&&define.amd?define(t):(e=e||self).boilerplate=t()}(this,(function(){"use strict";return()=>42}));
